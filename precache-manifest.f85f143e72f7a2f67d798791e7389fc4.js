0fbcfebd7ec84ea1b5b93b8ff7a9595a98a59ab8
Tahsin Çetin <tahsincetin86@gmal.com>
2021-07-15 13:19:54 +0300

    Updates

---
+++ precache-manifest.f85f143e72f7a2f67d798791e7389fc4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0cf1bdfd14d43bb20e0",
+    "url": "/optimalui-web/static/css/main.db6e7ca4.chunk.css"
+  },
+  {
+    "revision": "a0cf1bdfd14d43bb20e0",
+    "url": "/optimalui-web/static/js/main.5d509893.chunk.js"
+  },
+  {
+    "revision": "6ac8c46398a42127c69d",
+    "url": "/optimalui-web/static/js/runtime~main.a11e2189.js"
+  },
+  {
+    "revision": "f920d3c61bff45493183",
+    "url": "/optimalui-web/static/css/2.8bf39a0e.chunk.css"
+  },
+  {
+    "revision": "f920d3c61bff45493183",
+    "url": "/optimalui-web/static/js/2.9a7c10e3.chunk.js"
+  },
+  {
+    "revision": "ea08acdca9f8f2238d7740f0a3ed840f",
+    "url": "/optimalui-web/static/media/icon-clipboard.ea08acdc.svg"
+  },
+  {
+    "revision": "56632492c3a7a89bb0a64a83d9c5970e",
+    "url": "/optimalui-web/static/media/icon-flask.56632492.svg"
+  },
+  {
+    "revision": "d5889853bb80e0bfec003ea8546ef433",
+    "url": "/optimalui-web/static/media/sample_img_icon.d5889853.png"
+  },
+  {
+    "revision": "3313de67ccfbe9a32df9ae4b004e4d66",
+    "url": "/optimalui-web/static/media/light.3313de67.jpeg"
+  },
+  {
+    "revision": "7085058d349a5101d4404a9ccca7f15a",
+    "url": "/optimalui-web/static/media/avatar_.7085058d.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/optimalui-web/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/optimalui-web/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/optimalui-web/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/optimalui-web/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/optimalui-web/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7357745905a0c238489fee6097c9662c",
+    "url": "/optimalui-web/static/media/ProximaNova-Light-webfont.73577459.woff2"
+  },
+  {
+    "revision": "e382c667a81127659f464991b4013dfd",
+    "url": "/optimalui-web/static/media/ProximaNova-Reg-webfont.e382c667.woff2"
+  },
+  {
+    "revision": "814c3179f5679ece576f8dca33871775",
+    "url": "/optimalui-web/index.html"
+  }
+];
